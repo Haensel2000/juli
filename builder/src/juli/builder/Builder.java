d258c1e2124d99f730ff5ca7e191233a999732cc
Hannes Widmoser <widmoser@gmail.com>
2012-10-11 22:18:01 +0200

    Configuration file for builder.
    Closes #4

--- builder/src/juli/builder/Builder.java
+++ builder/src/juli/builder/Builder.java
@@ -1,13 +1,14 @@
 package juli.builder;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collection;
-
-import javax.swing.filechooser.FileNameExtensionFilter;
+import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -17,8 +18,10 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class Builder {
-
-	private static final String JLC = "../compiler/build/jlc";
+	
+	private static final String CONFIGURATION_FILE = "configuration.properties";
+	private static final String KEY_COMPILER = "compiler";
+	private static final String KEY_LINKER = "linker";
 
 	static void redirect(InputStream in, OutputStream out) throws IOException {
 		while (true) {
@@ -31,11 +34,14 @@ public class Builder {
 
 	private Collection<File> objects;
 	private File output;
+	private File buildDir;
+	
+	Properties configuration;
 
 	private File getOutputFile(File filePath) throws IOException {
 		String fileBaseName = filePath.getName();
 		fileBaseName = fileBaseName.substring(0, fileBaseName.lastIndexOf('.'));
-		return new File(System.getProperty("user.dir") + '/' + fileBaseName + ".o");
+		return new File(buildDir, fileBaseName + ".o");
 	}
 
 	private int executeCommand(String command, File dir) throws IOException, InterruptedException {
@@ -53,7 +59,7 @@ public class Builder {
 			}
 		} else {
 			File objectFile = getOutputFile(file);
-			String command = new File(JLC).getCanonicalPath() + " " + file.getCanonicalPath() + " -o "
+			String command = configuration.getProperty(KEY_COMPILER) + " " + file.getCanonicalPath() + " -o "
 					+ objectFile.getCanonicalPath();
 			System.out.println(command);
 			int res = executeCommand(command, file.getParentFile());
@@ -65,7 +71,7 @@ public class Builder {
 
 	public void link() throws IOException, InterruptedException {
 		StringBuilder command = new StringBuilder();
-		command.append("g++");
+		command.append(configuration.getProperty(KEY_LINKER));
 		command.append(" -o ");
 		command.append(output.toString());
 		for (File f : objects) {
@@ -83,15 +89,20 @@ public class Builder {
 		}
 	}
 
-	public Builder(File output) {
+	public Builder(File output, File buildDir) throws FileNotFoundException, IOException {
 		objects = new ArrayList<File>();
 		this.output = output;
+		this.buildDir = buildDir;
+		
+		configuration = new Properties();
+		configuration.load(new FileReader(CONFIGURATION_FILE));
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException, ParseException, BuildException {
 		Options options = new Options();
 		options.addOption("o", "output", true, "output file name");
 		options.addOption("h", "help", false, "print this message");
+		options.addOption("b", "build-dir", true, "build directory");
 		
 		CommandLineParser cliParser = new GnuParser();
 		CommandLine commandLine = cliParser.parse(options, args);
@@ -103,9 +114,15 @@ public class Builder {
 			formatter.printHelp(helpString , options );
 			return;
 		}
-		File output = new File("a.out");
+		
+		File buildDir = new File(".");
+		if (commandLine.hasOption('b')) {
+			buildDir = new File(commandLine.getOptionValue('b'));
+		}
+		
+		File output = new File(buildDir, "a.out");
 		if (commandLine.hasOption('o')) {
-			output = new File(commandLine.getOptionValue('o'));
+			output = new File(buildDir, commandLine.getOptionValue('o'));
 		}
 		
 		if (commandLine.getArgList().isEmpty()) {
@@ -114,11 +131,12 @@ public class Builder {
 			return;
 		}
 
-		Builder b = new Builder(output);
+		Builder b = new Builder(output, buildDir);
 		for (String s : commandLine.getArgs()) {
 			b.compile(new File(s));
 		}
 		b.link();
-		b.removeObjects();
+		
+		//b.removeObjects();
 	}
 }
